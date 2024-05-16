070c4c8973728c996ff778ab39adc9c325472c88
jufu87 <168488450+jufu87@users.noreply.github.com>
2024-05-16 20:00:02 +0200

    Add player input
    Add player input for a choice between rock, paper or scissors.
    Otherwise returns a message that choice is invalid and restarts.

--- index.js
+++ index.js
@@ -4,24 +4,49 @@ console.log("Rock, Paper, Scissors");
 
 // Computer choice:
 //  getComputerChoice will RANDOMLY RETURN one of the following string values: "rock", "paper", "scissors".
-//      RETURN a RANDOM value of 0 - 1
-//      MULTIPLY that value into 1 - 3    
-//      Assign a STRING to that value upon RETURN
+//      Generate a random value from 0 to 1
+//      Convert that value until you get a number from 1 to 3    
+//      Print a STRING according to that value
 
 function getComputerChoice () {
     
     let computerDice = Math.floor(Math.random() * 3) + 1;
-    console.log(computerDice);
+    //console.log(computerDice);
     
     if (computerDice === 1) {
-        console.log("Computer has Rock");
+        console.log("Computer played Rock");
     } else if (computerDice === 2) {
-        console.log("Computer has Paper");
+        console.log("Computer played Paper");
     } else {
-        console.log("Computer has Scissors");
+        console.log("Computer played Scissors");
+    }
+}
+//console.log("This is a console.log to test scope. Computer roll = " + computerDice);      --This does not work because computerDice is not in the global scope?
+
+
+// -------------------------------------------
+
+
+// Player choice:
+//      When the user inputs one of three string choices
+//      Make sure it is not case-sensitive   
+//      Print a new string according to the choice
+//      If user inputs invalid choice
+//      Notify and restart input
+
+function getHumanChoice () {
+    let humanChoice = prompt("Rock, Paper or Scissors?");
+    if (humanChoice.toLowerCase() === "rock") {
+        console.log("You played Rock");
+    } else if (humanChoice.toLowerCase() === "paper") {
+        console.log("You played Paper");
+    } else if (humanChoice.toLowerCase() === "scissors") {
+        console.log("You played Scissors")
+    } else {
+        alert("Not a valid choice!");
+        getHumanChoice();
     }
 }
 
+getHumanChoice()
 getComputerChoice()
-//console.log("This is console log 2 to test scope. X equals: " + x);           This does not work because x is not in the global scope?
-
