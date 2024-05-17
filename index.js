23df37686ca37b53737d2699566faf86a636f79e
jufu87 <168488450+jufu87@users.noreply.github.com>
2024-05-17 20:25:31 +0200

    Clean up code

--- index.js
+++ index.js
@@ -1,52 +1,62 @@
 console.log("Rock, Paper, Scissors");
 
-// Pseudocode:
+let humanScore = 0;
+let computerScore = 0;
+const choices = ["rock", "paper", "scissors"];
 
 // Computer choice:
 //  getComputerChoice will RANDOMLY RETURN one of the following string values: "rock", "paper", "scissors".
 //      Generate a random value from 0 to 1
-//      Convert that value until you get a number from 1 to 3    
-//      Print a STRING according to that value
+//      Round and multiply that value until you get a number from 0 to 2    
+//      Assign that value to a string via a list
+//      
 
-function getComputerChoice () {
+function getComputerChoice() {
     
-    let computerDice = Math.floor(Math.random() * 3) + 1;
-    //console.log(computerDice);
-    
-    if (computerDice === 1) {
-        console.log("Computer played Rock");
-    } else if (computerDice === 2) {
-        console.log("Computer played Paper");
-    } else {
-        console.log("Computer played Scissors");
-    }
-}
-//console.log("This is a console.log to test scope. Computer roll = " + computerDice);      --This does not work because computerDice is not in the global scope?
+    let computerInput = choices[Math.floor(Math.random() * 3)];
+    // Originally this was "let computerResult". It worked, but
+    // I was not sure how a LET inside a function of an already assigned 
+    // global CONST behaves so I made a new variable: "computerInput".
+    // Why did this work?
+    // This is something that needs research
 
-
-// -------------------------------------------
-
-
-// Player choice:
-//      When the user inputs one of three string choices
-//      Make sure it is not case-sensitive   
-//      Print a new string according to the choice
-//      If user inputs invalid choice
-//      Notify and restart input
-
-function getHumanChoice () {
-    let humanChoice = prompt("Rock, Paper or Scissors?");
-    if (humanChoice.toLowerCase() === "rock") {
-        console.log("You played Rock");
-    } else if (humanChoice.toLowerCase() === "paper") {
-        console.log("You played Paper");
-    } else if (humanChoice.toLowerCase() === "scissors") {
-        console.log("You played Scissors")
-    } else {
-        alert("Not a valid choice!");
-        getHumanChoice();
-    }
+    return computerInput;
 }
 
-getHumanChoice()
-getComputerChoice()
+//  Write a prompt with three valid inputs
+//  Loop function if input is invalid
+
+function getHumanChoice() {
+    
+    let humanInput = prompt("Rock, Paper or Scissors?");
+    // if (humanInput.toLowerCase() === "rock" || "paper" || "scissors") {
+    //     return humanInput;
+    // } else {
+    //     alert("not a valid choice");
+    // }
+    // Was trying to be clean but this did not work. Not enough knowledge of syntax.
+
+    if (humanInput.toLowerCase() === "rock") {
+        return humanInput.toLowerCase();
+    } else if (humanInput.toLowerCase() === "paper") {
+        return humanInput.toLowerCase();
+    } else if (humanInput.toLowerCase() === "scissors") {
+        return humanInput.toLowerCase();
+    } else {
+        alert("Invalid choice! Try again.");
+        return getHumanChoice();
+    }
+
+}
+
+function playRound(humanChoice, computerChoice) {
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log("Computer selected " + computerSelection);
+console.log("You selected " + humanSelection);
