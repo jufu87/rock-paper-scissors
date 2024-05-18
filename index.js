595a095fde7ba55fa20df86d383619e5604256e5
jufu87 <168488450+jufu87@users.noreply.github.com>
2024-05-18 15:32:18 +0200

    Clean up code and comments
    Clean up code, comments and add additional pseudocode
    that was initially done on paper.

--- index.js
+++ index.js
@@ -3,27 +3,32 @@ let round = 1;
 
 const choices = ["rock", "paper", "scissors"];
 
-// Computer choice:
+
+//  PSEUDOCODE:
+//  Computer choice:
 //  getComputerChoice will RANDOMLY RETURN one of the following string values: "rock", "paper", "scissors".
 //      Generate a random value from 0 to 1
 //      Round and multiply that value until you get a number from 0 to 2    
-//      Assign that value to a string via a list
-//      
+//      Assign that value to a string via a list. 
 
 function getComputerChoice() {
     
     let computerInput = choices[Math.floor(Math.random() * 3)];
-    // Originally this was "let computerSelection". It worked, but
+    // ^^ Originally this was "let computerSelection". It worked, but
     // I was not sure how a LET inside a function of an already assigned 
     // global CONST behaves so I made a new variable: "computerInput".
     // Why did computerSelection work?
     // This needs research
+    // computerSelection and humanSelection was later moved inside the playGame function.
 
     return computerInput;
 }
 
-//  Write a prompt with three valid inputs
-//  Loop function if input is invalid
+//  PSEUDOCODE:
+//  When user writes into a prompt
+//  convert and accept any combinations of lower and upper cases into lowercase.
+//  If user inputs something outside of the 3 valid strings, repeat the prompt with an error message
+//  If input is valid, RETURN the input as a string.
 
 function getHumanChoice() {
     
@@ -51,82 +56,57 @@ function getHumanChoice() {
 }
 
 
-// Include pseudocode here. I kind of cheated and skipped that.
-// Didn't know how to formulate my plan (which you would say means I
-// did not understand it, which seems to be a basic misunderstanding
-// of how the brain works).
-// There are ppl out there who have never learned or think in a language at all!
-// They are just as smart!
-
-
-
-
-//  Move the function and the score variables within the playRound function
+//  PSEUDOCODE:
+//  Move the playRound function and the score variables within the playGame function
 //  When a round finishes increment a variable on how many rounds have been played by 1
-//  Create a loop that stops once that variable reaches 5
+//  Create a loop that stops when said variable reaches 5
 
 function playGame() {
     let humanScore = 0
     let computerScore = 0
     
-   
-    
-    function playRound(humanChoice, computerChoice) {
-        console.log("Computer selected " + computerChoice);
-        console.log("You selected " + humanChoice);
-        round++;
-        
-        
-        if (humanChoice === computerChoice) {
-            return game = "Tie";
-        } else {
-            switch(humanChoice)  {
-                case "rock":
-                    game = (computerChoice === "scissors") ? "You won": "You lost";
-                    break;
-                case "paper":
-                    game = (computerChoice === "rock") ? "You won": "You lost";
-                    break;
-                case "scissors":
-                    game = (computerChoice === "paper") ? "You won": "You lost";
-                    break;
-            // if i get rid of "game" can I include the Score increments within the ternery syntax?
-            // When I tried to include it, it logged a "0" at the end of the string.
-            // how do I then RETURN the appropriate results?
+        function playRound(humanChoice, computerChoice) {
+            console.log("Computer selected " + computerChoice);
+            console.log("You selected " + humanChoice);
+            round++;
+            
+            if (humanChoice === computerChoice) {
+                return game = "Tie";
+            } else {
+                switch(humanChoice)  {
+                    case "rock":
+                        game = (computerChoice === "scissors") ? "You won": "You lost";
+                        break;
+                    case "paper":
+                        game = (computerChoice === "rock") ? "You won": "You lost";
+                        break;
+                    case "scissors":
+                        game = (computerChoice === "paper") ? "You won": "You lost";
+                        break;
+                // if i get rid of "game" can I include the Score increments within the ternery syntax?
+                // When I tried to include it, it logged a "0" at the end of the string.
+                // how do I then RETURN the appropriate results?
+                // Also, I COULD make a giant long "if else" statement (9 times!) but that seems silly, trying to use ternary/switch
+                }
             }
+            
+            if (game === "You won") {
+                humanScore++;
+            } else if (game === "You lost") {
+                computerScore++;
+            }
+            return game;
         }
-        
-        
-        
-        if (game === "You won") {
-            humanScore++;
-        } else if (game === "You lost") {
-            computerScore++;
-        }
-        return game;
     
-    
-    
-        // I COULD make a giant long "if else" statement (9 times!) but that seems silly, trying to use ternary/switch
-    
-    
-    
-    }
-    
-
-
         // This is the loop to call playRound 5 times:
-
         while (round !== 6) {
             const humanSelection = getHumanChoice();
             const computerSelection = getComputerChoice(); 
             playRound(humanSelection, computerSelection);
-    
             console.log(game);
-                
-    
             console.log("Computer score = " + computerScore);
             console.log("Your score = " + humanScore);
+            
             if (round !== 6) {
                 console.log("Next up round " + round);
             } else {
@@ -134,18 +114,13 @@ function playGame() {
             }
         }
     
-
         if (humanScore > computerScore) {
         console.log("YOU HAVE WON THE GAME! CONGRATULATIONS!");
         } else if (humanScore < computerScore) {
-            console.log("COMPUTER HAS WON THE GAME! COMMISERATIONS!");
+            console.log("COMPUTER HAS WON THE GAME! BETTER LUCK NEXT TIME!");
         } else {
             console.log("THE GAME WAS A TIE! HOW BORING!");
         }
     }
 
- 
-playGame(); 
-
-
-
+playGame();
